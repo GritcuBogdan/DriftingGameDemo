019b92636951ebe267b254bd9c6cf8857c6ee8ab
Grițcu Bogdan <47895616+GritcuBogdan@users.noreply.github.com>
2024-03-24 13:39:19 +0200

    Update WheelAlignment.cs

--- Assets/Demo/Scripts/WheelAlignment.cs
+++ Assets/Demo/Scripts/WheelAlignment.cs
@@ -15,16 +15,15 @@ public class WheelAlignment : MonoBehaviour {
 
     RaycastHit hit;
 	
-	// Update is called once per frame
+	
 	void Update ()
     {
         alignMeshToCollider();
 	}
 
-    //Rotates graphical wheel and updates its position
     void alignMeshToCollider()
     {
-        //Updates graphical wheel position by raycasting beneath the wheel with a max distance equaling the suspension distance and seeing if any ground is hit
+       
         if (Physics.Raycast(wheelCol.transform.position, -wheelCol.transform.up, out hit, wheelCol.suspensionDistance + wheelCol.radius))
         {
             wheelGraphic.transform.position = hit.point + wheelCol.transform.up *  wheelCol.radius;
@@ -34,7 +33,7 @@ public class WheelAlignment : MonoBehaviour {
             wheelGraphic.transform.position = wheelCol.transform.position - (wheelCol.transform.up * wheelCol.suspensionDistance);
         }
 
-        //Sees if wheel is steerable, if yes then applies steering angle
+        
         if (steerable)
         {
             wheelCol.steerAngle = steeringAngle;
