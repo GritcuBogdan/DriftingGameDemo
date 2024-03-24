069cff75fece9835439be4c9f91f76ff2c73a140
Grițcu Bogdan <47895616+GritcuBogdan@users.noreply.github.com>
2024-03-24 13:38:41 +0200

    Update CarDamage.cs

--- Assets/Demo/Scripts/CarDamage.cs
+++ Assets/Demo/Scripts/CarDamage.cs
@@ -1,15 +1,11 @@
-///--------------------------------------------------------------------------------------------
-/// Simple Car Damage system By Ciorbyn Studio https://www.youtube.com/c/CiorbynStudio
-/// Tutorial link: https://youtu.be/l04cw7EChpI
-/// -------------------------------------------------------------------------------------------
-/// 
+
 using UnityEngine;
 
 public class CarDamage : MonoBehaviour 
 {
-	public float maxMoveDelta = 1.0f; // maximum distance one vertice moves per explosion (in meters)
+	public float maxMoveDelta = 1.0f; 
 	public float maxCollisionStrength = 50.0f;
-	public float YforceDamp = 0.1f; // 0.0 - 1.0
+	public float YforceDamp = 0.1f; 
 	public float demolutionRange = 0.5f;
 	public float impactDirManipulator = 0.0f;
 	public MeshFilter[] MeshList;
@@ -17,7 +13,7 @@ public class CarDamage : MonoBehaviour
 	private MeshFilter[] meshfilters;
 	private float sqrDemRange;
 
-    //Save Vertex Data
+    
     private struct permaVertsColl
     {
         public Vector3[] permaVerts;
@@ -70,14 +66,14 @@ public class CarDamage : MonoBehaviour
 		
 		Vector3 colPointToMe = transform.position - collision.contacts[0].point;
 
-		// Dot = angle to collision point, frontal = highest damage, strip = lowest damage
+		
 		float colStrength = colRelVel.magnitude * Vector3.Dot(collision.contacts[0].normal, colPointToMe.normalized);
 
 		OnMeshForce( collision.contacts[0].point, Mathf.Clamp01(colStrength/maxCollisionStrength) );
 
 	}
 	
-	// if called by SendMessage(), we only have 1 param
+	
 	public void OnMeshForce( Vector4 originPosAndForce )
 	{
         OnMeshForce( (Vector3)originPosAndForce, originPosAndForce.w );
@@ -85,7 +81,7 @@ public class CarDamage : MonoBehaviour
 
 	public void OnMeshForce( Vector3 originPos, float force )
 	{
-        // force should be between 0.0 and 1.0
+        
         force = Mathf.Clamp01(force);
         
         for(int j=0; j<meshfilters.Length; ++j)
@@ -100,7 +96,7 @@ public class CarDamage : MonoBehaviour
                 Vector3 flatVertToCenterDir = transform.position - vertWorldPos;
                 flatVertToCenterDir.y = 0.0f;
                    
-                // 0.5 - 1 => 45° to 0°  / current vertice is nearer to exploPos than center of bounds
+                
                 if( originToMeDir.sqrMagnitude < sqrDemRange ) //dot > 0.8f )
                 {
                 	float dist = Mathf.Clamp01(originToMeDir.sqrMagnitude/sqrDemRange);
